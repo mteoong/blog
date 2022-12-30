e45adbad3a3ff0acbffab3f7d6671fc1aab8b623
Marc Teo Ong <marc.teoong@berkeley.edu>
2022-12-30 12:33:21 +0800

    Updates

---
+++ precache-manifest.ac5c2d027138941e7706d8bac7529e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce70a0e524212e6b8a7d6157ac1a7211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87f24d13a286fe928f60",
+    "url": "/static/css/main.5af80af5.chunk.css"
+  },
+  {
+    "revision": "2501891b82dcd6dbb3ce",
+    "url": "/static/js/2.f6fb394f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.f6fb394f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f24d13a286fe928f60",
+    "url": "/static/js/main.dd8b4d72.chunk.js"
+  },
+  {
+    "revision": "6640ba6dc6591d31438c",
+    "url": "/static/js/runtime-main.37587648.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.79f9af27f87782dfcd839b5e44e86d0d.js"
+  "/precache-manifest.ac5c2d027138941e7706d8bac7529e27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
