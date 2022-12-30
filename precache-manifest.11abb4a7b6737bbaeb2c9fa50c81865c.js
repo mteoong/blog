e0fcec230e1c60fb21a0919b2f0bf2b11fe0eb00
Marc Teo Ong <marc.teoong@berkeley.edu>
2022-12-30 11:54:45 +0800

    Updates

---
+++ precache-manifest.11abb4a7b6737bbaeb2c9fa50c81865c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71357683f278f0cfdeefe4bb274de2ea",
+    "url": "/marc_2.0/index.html"
+  },
+  {
+    "revision": "f0ed2503fa9ce785a13d",
+    "url": "/marc_2.0/static/css/main.5af80af5.chunk.css"
+  },
+  {
+    "revision": "2501891b82dcd6dbb3ce",
+    "url": "/marc_2.0/static/js/2.f6fb394f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/marc_2.0/static/js/2.f6fb394f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ed2503fa9ce785a13d",
+    "url": "/marc_2.0/static/js/main.448dfd86.chunk.js"
+  },
+  {
+    "revision": "453518e8c3b233c61c93",
+    "url": "/marc_2.0/static/js/runtime-main.49a613ca.js"
+  }
+]);
